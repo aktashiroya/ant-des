06f8e86c4e7f405bea352199d5941c9e2855c51b
aktashiroya <akta8758@gmail.com>
2022-12-02 07:46:40 +0530

    ant-des

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+import {
+  HomeOutlined,
+  LoadingOutlined,
+  SettingFilled,
+  SmileOutlined,
+  SyncOutlined,
+} from '@ant-design/icons';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button type="primary">Primary Button</Button>
+      <br />
+      <button type="primary">primary</button>
+      <br />
+      <button type="primary" disabled>primary(disabled)</button>
+      {/* <div><LogoutOutlined /></div> */}
+      <HomeOutlined />
+    <SettingFilled />
+    <SmileOutlined />
+    <SyncOutlined spin />
+    <SmileOutlined rotate={80} />
+    <LoadingOutlined />
+
+    </>
   );
 }
 
